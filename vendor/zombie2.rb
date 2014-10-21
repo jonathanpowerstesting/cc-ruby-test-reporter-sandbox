845c8e7f3544b9533c24f7d5139a32ef2c0b29a3
Jonathan Powers <bigblackrobot@gmail.com>
2014-10-21 10:51:15 -0400

    Blah

---
+++ vendor/zombie2.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+class Zombie2
+	attr_accessor :name
+	
+	def initialize
+		@name = 'Ash'
+	end
+	
+	def hello1 
+  		'Hello'  
+	end
+	
+	def hello2  
+  		'Hello'  
+	end
+	
+	def hello3  
+  		'Hello'  
+	end
+	
+	def hello4
+  		'Hello'  
+	end
+end
