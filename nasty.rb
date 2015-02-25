9a4e8843028d4ed42df64fb675f9b11da981ca18
Jonathan Powers <bigblackrobot@gmail.com>
2015-02-25 16:30:44 -0500

    Update nasty.rb

--- nasty.rb
+++ nasty.rb
@@ -1,43 +1,52 @@
 class Cat
   
+  # A ---------------------------------------------------------------------------------------------------------------------------------------
+
+  def cast_value(string)
+    return string unless string.is_a?(::String)
+    return if string.empty?
+
+    fast_string_to_time(string) || fallback_string_to_time(string)
+  end
+
   # B ---------------------------------------------------------------------------------------------------------------------------------------
 
-  def head(status, options = {})
-    options, status = status, nil if status.is_a?(Hash)
-    status ||= options.delete(:status) || :ok
-    location = options.delete(:location)
-    content_type = options.delete(:content_type)
+  # def head(status, options = {})
+  #   options, status = status, nil if status.is_a?(Hash)
+  #   status ||= options.delete(:status) || :ok
+  #   location = options.delete(:location)
+  #   content_type = options.delete(:content_type)
 
-    options.each do |key, value|
-      headers[key.to_s.dasherize.split('-').each { |v| v[0] = v[0].chr.upcase }.join('-')] = value.to_s
-    end
+  #   options.each do |key, value|
+  #     headers[key.to_s.dasherize.split('-').each { |v| v[0] = v[0].chr.upcase }.join('-')] = value.to_s
+  #   end
 
-    self.status = status
-    self.location = url_for(location) if location
+  #   self.status = status
+  #   self.location = url_for(location) if location
 
-    self.response_body = ""
+  #   self.response_body = ""
 
-    if include_content?(self.response_code)
-      self.content_type = content_type || (Mime[formats.first] if formats)
-      self.response.charset = false if self.response
-    else
-      headers.delete('Content-Type')
-      headers.delete('Content-Length')
-    end
-  end
+  #   if include_content?(self.response_code)
+  #     self.content_type = content_type || (Mime[formats.first] if formats)
+  #     self.response.charset = false if self.response
+  #   else
+  #     headers.delete('Content-Type')
+  #     headers.delete('Content-Length')
+  #   end
+  # end
 
-  private
-  # :nodoc:
-  def include_content?(status)
-    case status
-    when 100..199
-      false
-    when 204, 205, 304
-      false
-    else
-      true
-    end
-  end
+  # private
+  # # :nodoc:
+  # def include_content?(status)
+  #   case status
+  #   when 100..199
+  #     false
+  #   when 204, 205, 304
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
 
   # C ---------------------------------------------------------------------------------------------------------------------------------------
 
