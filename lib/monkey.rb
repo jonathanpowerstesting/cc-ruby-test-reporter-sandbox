bf5b3e26322eb91becf301a00737ba7c4995a4ed
Jonathan Powers <bigblackrobot@gmail.com>
2015-02-25 12:35:13 -0500

    Update monkey.rb

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -1,14 +1,4 @@
 class Monkey
-	
-	def foo
-    members.each do |member|
-      next if member.project.nil?
-      user_member = Member.find_by_project_id_and_user_id(member.project_id, user.id) || Member.new(:project_id => member.project_id, :user_id => user.id)
-      member.member_roles.each do |member_role|
-        user_member.member_roles << MemberRole.new(:role => member_role.role, :inherited_from => member_role.id)
-      end
-      user_member.save!
-    end
 
     members.each do |member|
       MemberRole.
@@ -27,16 +17,6 @@ class Monkey
 end
 
 class Axe
-  
-  def foo
-    members.each do |member|
-      next if member.project.nil?
-      user_member = Member.find_by_project_id_and_user_id(member.project_id, user.id) || Member.new(:project_id => member.project_id, :user_id => user.id)
-      member.member_roles.each do |member_role|
-        user_member.member_roles << MemberRole.new(:role => member_role.role, :inherited_from => member_role.id)
-      end
-      user_member.save!
-    end
 
     members.each do |member|
       MemberRole.
