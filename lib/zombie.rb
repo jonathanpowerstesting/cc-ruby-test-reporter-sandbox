294f04b96cacf87de941dee11c89063cc6e1c26e
Jonathan Powers <bigblackrobot@gmail.com>
2015-04-15 00:17:41 -0400

    Update zombie.rb

--- lib/zombie.rb
+++ lib/zombie.rb
@@ -29,23 +29,3 @@ class Zombie
   		'Hello'  
 	end
 end
-
-class Chicken
-	attr_accessor :name
-	
-	def initialize
-		@name = 'Ash'
-	end
-	
-	def hello1 
-  		'Hello'  
-	end
-
-	def hello2
-  		'Hello'  
-	end
-
-	def hello3
-  		'Hello'  
-	end
-end
