1dcd4a906927d6bab4993e1e14a10f08b2f1abf4
Jonathan Powers <bigblackrobot@gmail.com>
2015-05-28 12:50:23 -0400

    Blah.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'coveralls'
 Coveralls.wear!
-#require "codeclimate-test-reporter"
-#CodeClimate::TestReporter.start
 #require 'simplecov'
 #SimpleCov.start
 
